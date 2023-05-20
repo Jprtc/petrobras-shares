0b9619cde4e0192a2889fbdf25743708cf4559a4
Jprtc <jprtc8@gmail.com>
2023-05-20 02:49:25 -0300

    re-organized code for better SRP

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require("express");
 const path = require("path");
 
 // Import the file that fetches de csv file and "converts" it into a sqlite file
-const GetPetrobrasShares = require("./GetPetrobrasShares.js");
+const GetPetrobrasShares = require("./serverScripts/GetPetrobrasShares.js");
 // Fetch the data from the sqlite DB file
-const GetTableData = require("./GetTableData.js");
+const GetTableData = require("./serverScripts/GetTableData.js");
 
 // Using express and allowing the html to use the files in the /public path
 const app = express();
@@ -12,10 +12,9 @@ app.use(express.static("public"));
 app.set("views", path.join(__dirname, "views"));
 app.set("scripts", path.join(__dirname, "scripts"));
 
-const port = 3000;
-
 GetPetrobrasShares();
 
+const port = 3000;
 app.get("/", async (req, res) => {
   try {
     const tableName = "your_table";
